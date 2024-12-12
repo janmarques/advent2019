8b34fb539492666a9035b8aed10de036df5836e7
Jan Marques <jan@janmarques.net>
2024-12-12 18:41:56 +0100

    w

---
+++ ConsoleApp2/Program - Copy.cs
@@ -0,0 +1,36 @@
+﻿//var fullInput =
+//@"";
+
+//var smallInput =
+//@"";
+
+//var smallest = "";
+
+//var input = smallInput;
+////input = fullInput;
+////input = smallest;
+//var timer = System.Diagnostics.Stopwatch.StartNew();
+
+//var result = 0;
+
+//foreach (var line in input.Split(Environment.NewLine))
+//{
+
+//}
+
+//timer.Stop();
+//Console.WriteLine(result);
+//Console.WriteLine(timer.ElapsedMilliseconds + "ms");
+//Console.ReadLine();
+
+//void PrintGrid<T>(T[][] grid)
+//{
+//    for (int i = 0; i < grid.Length; i++)
+//    {
+//        for (int j = 0; j < grid[i].Length; j++)
+//        {
+//            Console.Write(grid[i][j]);
+//        }
+//        Console.WriteLine();
+//    }
+//}
